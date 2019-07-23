cec945694edfa103f600c9f9b8678eb36932328c
Malwina Mieczkowska <malwina@mieczkowski.eu>
2019-07-23 15:30:38 +0200

    connection is not defined

--- app/app.js
+++ app/app.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql'); 
-const post = require('./src/model/post');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: '43306',
-    user: 'root',
-    password: 'Secret123',
-    database: 'new_schema_23_07'
-});
-connection.query(
-    post.getPosts(),
-  //post.savePost(),
-  //`SELECT * FROM new_schema_23_07.posts;`,
-    (error, results, fields) => {
-        if (error) throw error;
-        console.log('The solution is: ', results);
-    }
-);
-connection.end();
+//const post = require('./src/model/post');
+ const connection = require('./src/model/connection');
+ //mysql.createConnection({
+//     host: 'localhost',
+//     port: '43306',
+//     user: 'root',
+//     password: 'Secret123',
+//     database: 'new_schema_23_07'
+// });
+// connection.query(
+//    // post.getPosts(),
+//   //post.savePost(),
+//   `SELECT * FROM new_schema_23_07.posts;`,
+//     (error, results, fields) => {
+//         if (error) throw error;
+//         console.log('The solution is: ', results);
+//     }
+// );
+// connection.end();
 //  let writePost = (null, 'Bla bla', 'bla-bla', 'hahahahahahah');
 //  savePost(writePost);
-getPosts();
+//getPosts();
--- app/src/model/connection.js
+++ app/src/model/connection.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql'); 
+const post = require('./post');
 const connection = mysql.createConnection({
     host: 'localhost',
     port: '43306',
@@ -6,4 +7,13 @@ const connection = mysql.createConnection({
     password: 'Secret123',
     database: 'new_schema_23_07'
 });
-
+//post.getPosts();
+   connection.query(
+        `SELECT * FROM new_schema_23_07.posts;`,
+           (error, results, fields) => {
+               if (error) throw error;
+               console.log('The solution is: ', results);
+           }
+       )
+connection.end();
+//getPosts();
