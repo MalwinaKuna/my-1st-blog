0a6db1ba8d3522915f0076dd3645db588c92c578
Malwina Mieczkowska <malwina@mieczkowski.eu>
2019-08-13 14:02:20 +0200

    endpoint posts added refactor

--- app/app.js
+++ app/app.js
@@ -1,8 +1,49 @@
 const post = require('./src/model/post');
 const express= require('express');
 const connection=require('./src/model/connection');
+const app = express();
+const bodyParser= require('body-parser');
+app.use(bodyParser.json());
 
+app.get('/',(req, res)=>{
+    res.send('Jest ok')
+});
+
+app.post('/posts', (req,res)=>{
+    let title =  req.body.title;
+    let slug = req.body.slug;
+    let content = req.body.content;
+
+    if(typeof title == 'string' && typeof slug == 'string' && typeof title == 'string') {
+        res.status(201);
+        res.json({
+            title: '<string>',
+            slug: '<string>',
+            content: '<string>'
+        })
+    }
+    const error=[];
+
+    if(typeof title !== 'string'){
+        error.push('title is not a string type');
+
+    }
+    if(typeof slug !== 'string'){
+        error.push('slug is not a string type');
+    }
+    if(typeof content !== 'string'){
+        error.push('content is not a string type');
+    }
+    if(error.length>0){
+        res.status(400);
+        res.json({
+            Errors: error
+        });
+    }
+    res.end();
+});
  post.getPosts();
  
 
  connection.end();
+ app.listen(8080, ()=>{'Listening on 8080'});
