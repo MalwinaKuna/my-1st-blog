b388718996f95cfc97e1467b6b779c942e3e0f76
Malwina Mieczkowska <malwina@mieczkowski.eu>
2019-08-14 10:57:07 +0200

    create post added

--- app/app.js
+++ app/app.js
@@ -1,20 +1,16 @@
 const post = require('./src/model/post');
-const express= require('express');
-const connection=require('./src/model/connection');
+const express = require('express');
+const connection = require('./src/model/connection');
 const app = express();
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.get('/',(req, res)=>{
-    res.send('Jest ok')
-});
-
-app.post('/posts', (req,res)=>{
-    let title =  req.body.title;
+app.post('/posts', (req, res) => {
+    let title = req.body.title;
     let slug = req.body.slug;
     let content = req.body.content;
 
-    if(typeof title == 'string' && typeof slug == 'string' && typeof title == 'string') {
+    if (typeof title == 'string' && typeof slug == 'string' && typeof title == 'string') {
         res.status(201);
         res.json({
             title: title,
@@ -22,19 +18,19 @@ app.post('/posts', (req,res)=>{
             content: content
         })
     }
-    const error=[];
+    const error = [];
 
-    if(typeof title !== 'string'){
+    if (typeof title !== 'string') {
         error.push('title is not a string type');
 
     }
-    if(typeof slug !== 'string'){
+    if (typeof slug !== 'string') {
         error.push('slug is not a string type');
     }
-    if(typeof content !== 'string'){
+    if (typeof content !== 'string') {
         error.push('content is not a string type');
     }
-    if(error.length>0){
+    if (error.length > 0) {
         res.status(400);
         res.json({
             Errors: error
@@ -43,25 +39,5 @@ app.post('/posts', (req,res)=>{
     res.end();
 });
 
-app.get('/posts/:id', (req, res)=> {
-    let id = parseInt(req.params.id);
-    if (!Number.isInteger(id)) {
-        res.status(404).json({ message: 'id must be a number' })
-    } else {
-        res.status(200).json({ 
-            id: id,
-            title: '<string>',
-            slug: '<string>',
-            content: '<string>'
-        });
-      
-    }
-    res.end();
-    
-});
-
- post.getPosts();
- 
- app.listen(8080, ()=> console.log('Listening on 8080'));
- connection.end();
- 
+app.listen(8080, () => console.log('Listening on 8080'));
+connection.end();
