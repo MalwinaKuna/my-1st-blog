f057efb7e28b12c45d48918c5c8a26e7b150e124
Malwina Mieczkowska <malwina@mieczkowski.eu>
2019-08-23 14:41:09 +0200

    updating post added

--- app/src/app.js
+++ app/src/app.js
@@ -5,10 +5,62 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.get('/posts', async(req, res)=>{
-            let posts = await postModel.getPosts();
-            res.json(posts);
-        });
+
+app.put('/posts/:id', async (req, res) => {
+
+    try {
+        if (isNaN(req.params.id)) {
+            res.status(400).json({
+                message: 'id must be a number'
+            });
+            return;
+        }
+        let postById = await postModel.getPost(req.params.id)
+        if (postById === null) {
+            res.status(404);
+            res.end();
+            return;
+        }
+        postById.title = await req.body.title;
+        postById.slug = await req.body.slug;
+        postById.content = await req.body.content;
+        
+        const error = [];
+        if (typeof postById.title !== 'string') {
+            error.push('title is not a string type');
+        }
+        if (typeof postById.slug !== 'string') {
+            error.push('slug is not a string type');
+        }
+        if (typeof postById.content !== 'string') {
+            error.push('content is not a string type');
+        }
+
+        if (await postModel.isSlugExist(postById)) {
+
+            error.push('The slug already exists');
+        }
+        if (error.length > 0) {
+            res.status(400);
+            res.json({
+                errors: error
+            });
+            return;
+        }
+        await postModel.updatePost(postById);
+        res.status(200).json(await postById);
+        return;
+    } catch (error) {
+        console.error(error.toString());
+        res.status(500);
+        res.end();
+        return;
+    }
+});
+app.get('/posts', async (req, res) => {
+    let posts = await postModel.getPosts();
+    res.json(posts);
+});
 app.get('/posts/:id', async (req, res) => {
 
     try {
