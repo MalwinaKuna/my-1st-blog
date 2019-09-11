b3576be0c9932bd65096a50bca8b02b10dceece2
Malwina Kuna Mieczkowska <46972410+MalwinaKuna@users.noreply.github.com>
2019-09-11 10:33:08 +0200

    Dockerfile and dependences added (#19)
    * Dockerfile and dependences added
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- app/src/model/connection.js
+++ app/src/model/connection.js
@@ -1,11 +1,11 @@
 const mysql= require('mysql');
 const connection= mysql.createConnection({
 
-    host: 'localhost',
-    port: '63306',
-    user: 'root',
-    password: 'Secret123',
-    database: 'new_schema_blog'
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 
 });
 module.exports=connection;
